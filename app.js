ca7a4b413ef8dc234acb0eea6993fb4aca68490f
juansalas0409 <juan.salas0409@gmail.com>
2021-09-16 00:01:11 -0300

    modelo de página e-commerce

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3000;
+const publicPath = path.join(__dirname, './public')
+
+app.use(express.static(publicPath));
+app.listen(port, () => {
+    console.log('Se esta corriendo el servidor en http://localhost:3000')
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/home.html'))
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/register.html'))
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/login.html'))
+});
